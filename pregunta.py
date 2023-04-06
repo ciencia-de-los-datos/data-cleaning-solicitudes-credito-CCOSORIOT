e1bcf5120c9c61049a6c13e64dd118d737bc2bea
CCOSORIOT <127028350+CCOSORIOT@users.noreply.github.com>
2023-04-06 14:26:48 -0500

    Lab 5
    Intento 1 Laboratorio 5

--- pregunta.py
+++ pregunta.py
@@ -13,8 +13,17 @@ def clean_data():
 
     df = pd.read_csv("solicitudes_credito.csv", sep=";")
 
-    #
-    # Inserte su código aquí
-    #
+    del df['Unnamed: 0']
+    df = df.dropna().drop_duplicates()
+
+    df['sexo'] = df['sexo'].apply(str.lower)
+    df['tipo_de_emprendimiento'] = df['tipo_de_emprendimiento'].apply(str.lower)
+    df['idea_negocio'] = df['idea_negocio'].apply(lambda x: x.lower().replace('_', ' ').replace('-', ' '))
+    df['barrio'] = df['barrio'].apply(lambda x: x.lower().replace('_', '-').replace('-', ' '))
+    df['línea_credito'] = df['línea_credito'].apply(lambda x: x.lower().replace('_', ' ').replace('-', ' '))
+    df['monto_del_credito'] = df['monto_del_credito'].str.replace(',', '').str.replace('$', '', regex=False).str.replace(' ', '').astype(float)
+    df['fecha_de_beneficio'] = pd.to_datetime(df['fecha_de_beneficio'], dayfirst=True)
+    
+    df = df.drop_duplicates().dropna()
 
     return df
