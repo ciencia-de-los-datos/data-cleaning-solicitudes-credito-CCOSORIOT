29addf309405af663126fea806fdca16575e8ff9
CCOSORIOT <127028350+CCOSORIOT@users.noreply.github.com>
2023-04-06 15:11:07 -0500

    Lab5 - V4
    Intento número 4 del laboratorio 5

--- pregunta.py
+++ pregunta.py
@@ -16,14 +16,14 @@ def clean_data():
     del df['Unnamed: 0']
     df = df.dropna().drop_duplicates()
 
-    df['sexo'] = df['sexo'].apply(str.lower)
-    df['tipo_de_emprendimiento'] = df['tipo_de_emprendimiento'].apply(str.lower)
-    df['idea_negocio'] = df['idea_negocio'].apply(lambda x: x.lower().replace('_', ' ').replace('-', ' '))
-    df['barrio'] = df['barrio'].apply(lambda x: x.lower().replace('_', '-').replace('-', ' '))
-    df['línea_credito'] = df['línea_credito'].apply(lambda x: x.lower().replace('_', ' ').replace('-', ' '))
+    df['sexo'] = df['sexo'].apply(str.lower).str.capitalize()
+    df['tipo_de_emprendimiento'] = df['tipo_de_emprendimiento'].apply(str.lower).str.capitalize()
+    df['idea_negocio'] = df['idea_negocio'].apply(lambda x: x.lower().replace('_', ' ').replace('-', ' ')).str.capitalize()
+    df['barrio'] = df['barrio'].apply(lambda x: x.lower().replace('_', '-').replace('-', ' ')).str.capitalize()
+    df['línea_credito'] = df['línea_credito'].apply(lambda x: x.lower().replace('_', ' ').replace('-', ' ')).str.capitalize()
     df['monto_del_credito'] = df['monto_del_credito'].str.replace(',', '').str.replace('$', '', regex=False).str.replace(' ', '').astype(float)
     df['fecha_de_beneficio'] = pd.to_datetime(df['fecha_de_beneficio'], dayfirst=True)
-
+    
     df = df.drop_duplicates().dropna()
     df.reset_index(drop = True)
 
